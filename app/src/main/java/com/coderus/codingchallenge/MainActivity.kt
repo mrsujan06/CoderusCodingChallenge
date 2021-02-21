3abacdf299cb44fc5776ce9e86ce359b9a3429e5
Sujan Thapa <mrsujan06@gmail.com>
2021-02-21 04:30:47 +0000

    Implemented Button to Switch between Light and Dark Theme

--- app/src/main/java/com/coderus/codingchallenge/MainActivity.kt
+++ app/src/main/java/com/coderus/codingchallenge/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.coderus.codingchallenge
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.coderus.codingchallenge.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        switchThemeButton()
+    }
+
+    private fun switchThemeButton() {
+        binding.themeSwitchBtn.setOnClickListener {
+            when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                Configuration.UI_MODE_NIGHT_YES ->
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                Configuration.UI_MODE_NIGHT_NO ->
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+
+        }
     }
 }
